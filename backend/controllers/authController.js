4bc0ad648f1d29770c83d96258a642120b06cc5c
Anas Nady <anasabdallahnady@gmail.com>
2023-10-19 14:03:52 +0300

    Create logout api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,3 +53,11 @@ export const signIn = asyncHandler(async (req, res, next) => {
 
   createSendToken(isUserExists, 200, res);
 });
+
+export const logout = (req, res) => {
+  res.cookie("jwt", "", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: "success" });
+};
