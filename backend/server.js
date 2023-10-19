10130299d7bf105c5f6e28f9a94c6c9bce28a930
Anas Nady <anasabdallahnady@gmail.com>
2023-10-19 09:31:11 +0300

    add error handling functionality

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/authRoute.js";
+import globalErrorHandler from "./controllers/errorController.js";
+import AppError from "./utils/appError.js";
 dotenv.config();
 
 const app = express();
@@ -18,6 +20,12 @@ mongoose
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
