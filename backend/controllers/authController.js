994f2d86cdf0dbadef5ffd7ffa66d6c9dc88ae50
Anas Nady <anasabdallahnady@gmail.com>
2023-10-19 12:33:49 +0300

    complete sign up page functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,10 +11,26 @@ export const signup = asyncHandler(async (req, res, next) => {
   }
 
   const hash = await bcrypt.hash(password, 10);
+
+  const emailIsExists = await User.findOne({ email });
+  const usernameIsExists = await User.findOne({ username });
+
+  if (emailIsExists && usernameIsExists) {
+    return next(
+      new AppError("Your enter email and username is already exists.", 400)
+    );
+  }
+
+  if (emailIsExists)
+    return next(new AppError("Your enter email is already exists.", 400));
+
+  if (usernameIsExists)
+    return next(new AppError("Your enter username is already exists.", 400));
+
   const newUser = await User.create({ username, email, password: hash });
 
   if (!newUser) {
-    return next(new AppError("invalid data input.!", 400));
+    return next(new AppError("invalid data input.!", 500));
   }
   res.status(201).json({ newUser, message: "User Created successfully." });
 });
