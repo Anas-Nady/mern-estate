8b6220481e890eb796c4165875117a4c18f30fec
Anas Nady <anasabdallahnady@gmail.com>
2023-10-19 13:58:10 +0300

    upgrade sign up functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,36 +1,28 @@
-import User from "./../models/userModel.js";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import User from "./../models/userModel.js";
+import createSendToken from "../utils/createSendToken.js";
 import AppError from "../utils/appError.js";
 
-export const signup = asyncHandler(async (req, res, next) => {
+export const signUp = asyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return next(new AppError("All data are required.", 400));
+    return next(new AppError("All data are required.", 500));
   }
 
   const hash = await bcrypt.hash(password, 10);
 
-  const emailIsExists = await User.findOne({ email });
-  const usernameIsExists = await User.findOne({ username });
+  const existsCheck = await User.findOne({ $or: [{ username }, { email }] });
 
-  if (emailIsExists && usernameIsExists) {
-    return next(
-      new AppError("Your enter email and username is already exists.", 400)
-    );
+  if (existsCheck) {
+    return next(new AppError("Your enter data is already exists.", 400));
   }
-
-  if (emailIsExists)
-    return next(new AppError("Your enter email is already exists.", 400));
-
-  if (usernameIsExists)
-    return next(new AppError("Your enter username is already exists.", 400));
 
   const newUser = await User.create({ username, email, password: hash });
 
   if (!newUser) {
     return next(new AppError("invalid data input.!", 500));
   }
-  res.status(201).json({ newUser, message: "User Created successfully." });
+  createSendToken(newUser, 201, res);
 });
