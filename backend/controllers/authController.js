9ca0539275a0d7d100f13fd36f35d2de507d5203
Anas Nady <anasabdallahnady@gmail.com>
2023-10-19 09:46:24 +0300

    handle error with sign up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,20 @@
 import User from "./../models/userModel.js";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import AppError from "../utils/appError.js";
 
-export const signup = asyncHandler(async (req, res) => {
+export const signup = asyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const hash = await bcrypt.hash(password, 10);
+  if (!username || !email || !password) {
+    return next(new AppError("All data are required.", 400));
+  }
 
+  const hash = await bcrypt.hash(password, 10);
   const newUser = await User.create({ username, email, password: hash });
 
+  if (!newUser) {
+    return next(new AppError("invalid data input.!", 400));
+  }
   res.status(201).json({ newUser, message: "User Created successfully." });
 });
